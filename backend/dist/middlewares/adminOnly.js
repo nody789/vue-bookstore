06e837b9ce8e6d0ed1aea519990f0d699f7c40e3
nody789 <nody789@gmail.com>
2026-08-25 20:57:55 +0800

    chore: 將 dist/ 和 generated/ 加入 .gitignore，移除已追蹤的編譯產物

--- backend/dist/middlewares/adminOnly.js
+++
@@ -1,7 +0,0 @@
-import { AppError } from '../types/index.js';
-export const adminOnly = (req, _res, next) => {
-    if (req.user?.role !== 'ADMIN') {
-        throw new AppError(403, 'FORBIDDEN', '需要管理員權限');
-    }
-    next();
-};
